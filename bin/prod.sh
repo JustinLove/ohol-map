da2deae399ba85144f34d710cf9461fc0b8b179f
Justin Love <git@JustinLove.name>
2021-02-20 10:38:18 -0600

    set cache control at origin

--- bin/prod.sh
+++ bin/prod.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 elm make src/MapUI.elm --output public/map-ui.js
-s3cmd sync public/* s3://wondible-com-onemap/
+s3cmd --add-header="Cache-Control:max-age=86400" --exclude public/index.html sync public/* s3://wondible-com-onemap/
+s3cmd --add-header="Cache-Control:no-cache" sync public/index.html s3://wondible-com-onemap/
--- bin/stage.sh
+++ bin/stage.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 elm make src/MapUI.elm --output public/map-ui.js
-s3cmd sync public/* s3://wondible-com-twomap/
+s3cmd --add-header="Cache-Control:max-age=600" --exclude public/index.html sync public/* s3://wondible-com-twomap/
+s3cmd --add-header="Cache-Control:no-cache" sync public/index.html s3://wondible-com-twomap/
